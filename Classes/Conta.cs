c0dd8f99ff57fc1abaf998fb6c1a2fecb60e70a7
Rodrigo-Tibirica <47817462+Rodrigo-Tibirica@users.noreply.github.com>
2021-09-23 18:02:21 -0300

    Update Conta.cs

--- Classes/Conta.cs
+++ Classes/Conta.cs
@@ -29,7 +29,6 @@ namespace DIO.Bank
             this.Saldo -= valorSaque;
 
             Console.WriteLine("Saldo atual da conta de {0} é {1}", this.Nome, this.Saldo);
-            // https://docs.microsoft.com/pt-br/dotnet/standard/base-types/composite-formatting
 
             return true;
 		}
